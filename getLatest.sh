4c161bd22e634cff7ba5230c1701ce70d64d55aa
Tony Tang <tonytang.dev@gmail.com>
2020-12-11 21:30:10 +1100

    Fix run latest script

--- getLatest.sh
+++ getLatest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 latestYear=`ls | grep '20.*' | tail -n 1`
-echo $(cd $latestYear && echo $latestYear/$(ls -v | tail -n 1)) >&2
+(cd $latestYear && echo $latestYear/$(ls -v | tail -n 1))
